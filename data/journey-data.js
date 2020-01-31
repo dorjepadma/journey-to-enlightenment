637f3a755243e1054c0e785174fb400e5efbf305
dorjepadma <dorjepadma@gmail.com>
2020-01-31 08:58:51 -0800

    morning save dont remember what I changed last night ;(

---
+++ data/journey-data.js
@@ -0,0 +1,12 @@
+//accumulating merit and wisdom
+const retreat = {
+    id: 'retreat',
+    title: 'Mountain Retreat'
+    map: {
+        top:'85%',
+        left:'44%'
+    },
+    image: 'yogi.jpg'
+    audio: 'mountain.wav'
+    
+}
